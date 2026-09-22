dbc9e845d81baf07edf165283a6345c7c5d096ef
BUAAZhangHaonan <zhn19375264@buaa.edu.cn>
2026-09-23 01:01:00 +0800

    remove experiments_archive entirely
    4 pre-final experiment families (coco4ch/dptd/distill/cross_distillation)
    plus the v317 source residue: the final tree was promoted to repo root in
    6510f4f8; everything here is iteration history recoverable from git.

--- experiments_archive/coco4ch_base_distilled_depth_tower_film_injection/source/magformer/models/common/transformer/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Transformer components."""
-
-from .decoder import SimpleTransformerDecoder
-from .multiscale_decoder import MultiScaleMaskedTransformerDecoder
-
-__all__ = ["SimpleTransformerDecoder", "MultiScaleMaskedTransformerDecoder"]
--- experiments_archive/coco4ch_base_distilled_depth_tower_film_injection/source/magformer/models/ops/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-MSDeformAttn modules.
-"""
-
-from .ms_deform_attn import MSDeformAttn
-
-__all__ = ['MSDeformAttn']
